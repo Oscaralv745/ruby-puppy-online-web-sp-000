a8cf09b4336acde7c9a02fb417df2206c139ba8a
Oscar Alvarez <gavinalv6@gmail.com>
2021-05-17 19:19:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,7 +7,7 @@ class Dog
 
   def initialize(name)
     @name = name
-    @@all << self
+    save 
   end
 
   def self.all
@@ -17,4 +17,8 @@ class Dog
   def self.print_all
     puts @@all.map { |dog| dog.name }
   end
+
+  def save
+    @@all << self
+  end  
 end
